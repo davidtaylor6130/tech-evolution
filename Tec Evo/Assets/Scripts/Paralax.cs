f0eb1e6604ac56398111b4d8dac5142132c2cfd0
davidtaylor613 <43450525+davidtaylor613@users.noreply.github.com>
2019-05-21 00:21:22 +0100

    Finished new layerd background

---
+++ Tec Evo/Assets/Scripts/Paralax.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paralax : MonoBehaviour {
+
+    public float Speed;
+    Vector3 startPos;
+    public float ResetSection;
+	
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Translate((new Vector3(-1, 0, 0)) * Speed * Time.deltaTime);
+
+        if (transform.position.x < ResetSection)
+        {
+             transform.position = startPos;
+        }
+    }
+}
