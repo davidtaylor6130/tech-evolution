5ef0026073c9bab5a9dfc9990d6c92ad329be7d4
davidtaylor613 <43450525+davidtaylor613@users.noreply.github.com>
2019-05-29 20:00:37 +0100

    Re-worked, movement

---
+++ Tec Evo/Assets/Scripts/MoveNodes.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveNodes : MonoBehaviour {
+
+    //Array to store all the node places on
+    GameObject[] array = new GameObject[7];
+
+    public GameObject Nodes; 
+    public GameObject NodesOne;
+    public GameObject NodesTwo;
+    public GameObject NodesThree;
+    public GameObject NodesFour;
+    public GameObject NodesFive;
+    public GameObject NodesSix;
+    public float MoveLeftAndRight;
+    public int StartingPosition;
+
+    bool HasBeenMoved;
+    int ammountMoved = 0;
+	 
+    void Start()
+    {
+        array[0] = Nodes;
+        array[1] = NodesOne;
+        array[2] = NodesTwo;
+        array[3] = NodesThree;
+        array[4] = NodesFour;
+        array[5] = NodesFive;
+        array[6] = NodesSix;
+    }
+
+    void OnMouseOver()
+    {
+        Debug.Log("over");
+
+        if (Input.GetMouseButton(0) == true)
+        {
+            if (HasBeenMoved == false)
+            {
+                Move();
+                HasBeenMoved = true;
+            }
+        }
+
+        if (Input.GetMouseButton(0) == false && HasBeenMoved == true)
+        {
+            HasBeenMoved = false;
+        }
+
+    }
+
+    void Move()
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            Debug.Log(i);
+            if (array[i].transform.localPosition.x > 0)
+            {
+                ammountMoved++;
+            }
+            else if (array[i].transform.localPosition.x > 0)
+            {
+                ammountMoved--;
+            }
+
+            array[i].transform.localPosition += new Vector3(MoveLeftAndRight, 0, 0);
+        }
+    }
+}
